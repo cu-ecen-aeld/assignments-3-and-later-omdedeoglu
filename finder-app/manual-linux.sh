60ef3962b0404d5c82219af80916114067f45b25
omdedeoglu <htepe.ozgunddoglu@gmail.com>
2026-01-29 11:50:18 -0800

    testing sysroot

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -17,9 +17,11 @@ CROSS_COMPILER_PATH=/tmp/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-a
 export PATH="$PATH:/tmp/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin"
 echo "pwd"; pwd
 echo "whoami"; whoami
+sysroot=$(${CROSS_COMPILE}gcc -print-sysroot)
+echo ${sysroot}
 #echo "cat /proc/mounts"; cat /proc/mounts
-echo "/tmp"; ls /tmp
-echo "cross compiler directory contents:"; ls ${CROSS_COMPILER_PATH}
+#echo "/tmp"; ls /tmp
+#echo "cross compiler directory contents:"; ls ${CROSS_COMPILER_PATH}
 
 if [ $# -lt 1 ]
 then
@@ -51,7 +53,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
+#cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -83,8 +85,8 @@ else
 fi
 
 # TODO: Make and install busybox
-#make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-#make CONFIG_PREFIX=${ramfs_dir} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${ramfs_dir} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 cd ${ramfs_dir}
 
 echo "Library dependencies"
@@ -92,10 +94,10 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-#sudo cp "${CROSS_COMPILER_PATH}/lib/ld-linux-aarch64.so.1" "${ramfs_dir}/lib/"
-#sudo cp "${CROSS_COMPILER_PATH}/lib64/libm.so.6" "${ramfs_dir}/lib64/"
-#sudo cp "${CROSS_COMPILER_PATH}/lib64/libresolv.so.2" "${ramfs_dir}/lib64/"
-#sudo cp "${CROSS_COMPILER_PATH}/lib64/libc.so.6" "${ramfs_dir}/lib64/"
+sudo cp "${sysroot}/lib/ld-linux-aarch64.so.1" "${ramfs_dir}/lib/"
+sudo cp "${sysroot}/lib64/libm.so.6" "${ramfs_dir}/lib64/"
+sudo cp "${sysroot}/lib64/libresolv.so.2" "${ramfs_dir}/lib64/"
+sudo cp "${sysroot}/lib64/libc.so.6" "${ramfs_dir}/lib64/"
 # TODO: Make device nodes
 #sudo mknod -m 666 ${ramfs_dir}/dev/null c 1 3
 #sudo mknod -m 666 ${ramfs_dir}/dev/console c 5 1
